32b4538af7f2cbe303a3fd27bbc49a5ea0d3cd55
AliKamar34 <aliekamar123@gmail.com>
2026-05-03 02:11:24 +0300

    Add authentication routes to AppRouter and rename reset password route

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,11 @@
 import 'package:go_router/go_router.dart';
 import 'package:sokan_msr/core/routing/app_routes.dart';
+import 'package:sokan_msr/features/auth/presentation/views/login_view.dart';
+import 'package:sokan_msr/features/auth/presentation/views/privacy_policy_view.dart';
+import 'package:sokan_msr/features/auth/presentation/views/reset_password_view.dart';
+import 'package:sokan_msr/features/auth/presentation/views/sign_up_view.dart';
+import 'package:sokan_msr/features/auth/presentation/views/terms_of_service_view.dart';
+import 'package:sokan_msr/features/auth/presentation/views/verify_view.dart';
 import 'package:sokan_msr/features/start/presentation/views/on_boarding_view.dart';
 import '../../features/start/presentation/views/splash_view.dart';
 
@@ -17,36 +23,36 @@ class AppRouter {
         name: AppRoutes.onboardingRoute,
         builder: (context, state) => const OnBoardingView(),
       ),
-      // GoRoute(
-      //   path: AppRoutes.loginRoute,
-      //   name: AppRoutes.loginRoute,
-      //   builder: (context, state) => const LoginView(),
-      // ),
-      // GoRoute(
-      //   path: AppRoutes.signupRoute,
-      //   name: AppRoutes.signupRoute,
-      //   builder: (context, state) => const SignUpView(),
-      // ),
-      // GoRoute(
-      //   path: AppRoutes.verifyRoute,
-      //   name: AppRoutes.verifyRoute,
-      //   builder: (context, state) => const VerifyView(),
-      // ),
-      // GoRoute(
-      //   path: AppRoutes.resetPasswordRoute,
-      //   name: AppRoutes.resetPasswordRoute,
-      //   builder: (context, state) => const ResetPasswordView(),
-      // ),
-      // GoRoute(
-      //   path: AppRoutes.privacyPolicyRoute,
-      //   name: AppRoutes.privacyPolicyRoute,
-      //   builder: (context, state) => const PrivacyPolicyView(),
-      // ),
-      // GoRoute(
-      //   path: AppRoutes.termsOfServiceRoute,
-      //   name: AppRoutes.termsOfServiceRoute,
-      //   builder: (context, state) => const TermsOfServiceView(),
-      // ),
+      GoRoute(
+        path: AppRoutes.loginRoute,
+        name: AppRoutes.loginRoute,
+        builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: AppRoutes.signupRoute,
+        name: AppRoutes.signupRoute,
+        builder: (context, state) => const SignUpView(),
+      ),
+      GoRoute(
+        path: AppRoutes.verifyRoute,
+        name: AppRoutes.verifyRoute,
+        builder: (context, state) => const VerifyView(),
+      ),
+      GoRoute(
+        path: AppRoutes.forgotPasswordRoute,
+        name: AppRoutes.forgotPasswordRoute,
+        builder: (context, state) => const ResetPasswordView(),
+      ),
+      GoRoute(
+        path: AppRoutes.privacyPolicyRoute,
+        name: AppRoutes.privacyPolicyRoute,
+        builder: (context, state) => const PrivacyPolicyView(),
+      ),
+      GoRoute(
+        path: AppRoutes.termsOfServiceRoute,
+        name: AppRoutes.termsOfServiceRoute,
+        builder: (context, state) => const TermsOfServiceView(),
+      ),
     ],
   );
 }
