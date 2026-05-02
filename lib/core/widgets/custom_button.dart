02cda0a5aa26c8408166c814b9a664d484510164
AliKamar34 <aliekamar123@gmail.com>
2026-05-02 21:43:14 +0300

    Add CustomButton widget for reusable button component

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sokan_msr/core/themes/app_colors.dart';
+import 'package:sokan_msr/core/themes/app_text_style.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.title,
+    this.onPressed,
+    this.borderColor,
+    this.backgroundColor,
+    this.icon,
+  });
+  final String title;
+  final void Function()? onPressed;
+  final Color? backgroundColor;
+  final Color? borderColor;
+  final Widget? icon;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(double.infinity, 68.h),
+        backgroundColor:
+            backgroundColor ??
+            Theme.of(context).extension<AppColors>()!.primaryColor,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: borderColor ?? backgroundColor ?? Colors.transparent,
+          ),
+          borderRadius: BorderRadius.circular(32.r),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          icon == null ? const SizedBox.shrink() : icon!,
+          icon == null ? const SizedBox.shrink() : const SizedBox(width: 6),
+          Expanded(
+            child: Text(
+              title,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              style: AppTextStyle.styleBold18.copyWith(
+                color:
+                    borderColor ??
+                    Theme.of(context).extension<AppColors>()!.whiteColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
